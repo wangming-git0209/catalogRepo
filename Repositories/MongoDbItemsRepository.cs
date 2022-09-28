499110110c85f0f6cb739da6018f6b50d347df05
wangming-git0209 <minhnnqgcd191322@fpt.edu.vn>
2022-09-28 21:54:57 +0700

    change

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,31 @@
+﻿using CATALOG.Entities;
+
+namespace CATALOG.Repositories;
+
+public class MongoDbItemsRepository : IItemsRepository 
+{
+    public Item GetItem(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Item> GetItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateItem(Item item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateItem(Item item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteItem(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
